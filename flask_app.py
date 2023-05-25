9c860b676c76977865ba238d1656ef2981ae7bf2
felipebaitelman <felipebaitelman@gmail.com>
2023-05-25 15:13:10 -0400

    Dummy prompt retunring dictionary implementation

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,8 @@
 import os
 import json
+import seaborn as sns
+import pandas as pd
+
 from flask import Flask, render_template, request, jsonify
 import openai
 from openai.error import RateLimitError
@@ -13,8 +16,40 @@ def index():
 
 @app.route('/gpt4', methods=['GET', 'POST'])
 def gpt4():
+
     user_input = request.args.get('user_input') if request.method == 'GET' else request.form['user_input']
-    messages = [{"role": "user", "content": user_input}]
+
+    latin_american_countries = [
+        "Argentina",
+        "Bolivia",
+        "Brazil",
+        "Chile",
+        "Colombia",
+        "Costa Rica",
+        "Cuba",
+        "Dominican Republic",
+        "Ecuador",
+        "El Salvador",
+        "Guatemala",
+        "Haiti",
+        "Honduras",
+        "Mexico",
+        "Nicaragua",
+        "Panama",
+        "Paraguay",
+        "Peru",
+        "Puerto Rico",
+        "Uruguay",
+        "Venezuela"
+    ]
+
+    prompt = "What was the " + user_input + " for the following countries: " + str(latin_american_countries) + " in 2010?" + \
+             " If you can't provide the data, just use a random number. Data will be used for a personal project. Only return results as a Pandas Dataframe."
+
+    prompt = "Return text formatted as a python dictionary with the following countries: " + str(latin_american_countries) + \
+             " and an associated random number. Provide only the dictionary, not the code implementation."
+
+    messages = [{"role": "user", "content": prompt}]
 
     try:
         response = openai.ChatCompletion.create(
