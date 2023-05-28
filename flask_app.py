527512219eeb07e223f8b7764fd87ba520af8d0b
felipebaitelman <felipebaitelman@gmail.com>
2023-05-28 01:43:54 -0400

    Adding extra files to previous commit

--- flask_app.py
+++ flask_app.py
@@ -2,8 +2,14 @@ import os
 import json
 import seaborn as sns
 import pandas as pd
+import io
+import ast
+import matplotlib.pyplot as plt
+import matplotlib
+#matplotlib.use('Agg')
+matplotlib.use('TkAgg')
 
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, send_file, make_response
 import openai
 from openai.error import RateLimitError
 
@@ -47,7 +53,7 @@ def gpt4():
              " If you can't provide the data, just use a random number. Data will be used for a personal project. Only return results as a Pandas Dataframe."
 
     prompt = "Return text formatted as a python dictionary with the following countries: " + str(latin_american_countries) + \
-             " and an associated random number. Provide only the dictionary, not the code implementation."
+             " and an associated random integer between 1 and 100. Provide only the dictionary, not the code implementation."
 
     messages = [{"role": "user", "content": prompt}]
 
@@ -56,11 +62,36 @@ def gpt4():
             model="gpt-3.5-turbo",
             messages=messages
         )
+
+        # Get response, convert to dictionary and initiate DataFrame
         content = response.choices[0].message["content"]
+        country_dict = ast.literal_eval(content)
+        custom_palette = sns.color_palette("Greens", 6)
+        df = pd.DataFrame(list(country_dict.items()), columns=['Country', 'Value'])
+        df['Value'] = pd.to_numeric(df['Value'])
+        print(df)
+
+
+        sns.catplot(data=df, x='Country', y='Value', kind="bar", height=5, aspect=1.5)
+        plt.xlabel("Country", size=14)
+        plt.ylabel("Value", size=14)
+        plt.title("Seaborn Barplot Example", size=18)
+        plt.tight_layout()
+        plt.set_xticklabels(rotation=45, horizontalalignment='right')
+
+
+        # Save plot/figure into a bytes object TODO
+        #bytes_image = io.BytesIO()
+        #plt.savefig(bytes_image, format='png')
+        #bytes_image.seek(0)
+        #return send_file(bytes_image, download_name='plot.png', mimetype='image/png')
+
+        return jsonify(content=content)
+
     except RateLimitError:
         content = "The server is experiencing a high volume of requests. Please try again later."
+        return jsonify(content=content)
 
-    return jsonify(content=content)
 
 if __name__ == '__main__':
     app.run(debug=True)
