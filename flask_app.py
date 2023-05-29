476b511f37a85ff08c393d260615287f9ce305f2
felipebaitelman <felipebaitelman@gmail.com>
2023-05-28 22:50:22 -0400

    Implementation of seaborn rendering in html

--- flask_app.py
+++ flask_app.py
@@ -2,28 +2,30 @@ import os
 import json
 import seaborn as sns
 import pandas as pd
-import io
+from io import BytesIO
 import ast
 import matplotlib.pyplot as plt
 import matplotlib
-#matplotlib.use('Agg')
-matplotlib.use('TkAgg')
+matplotlib.use('Agg')
+import base64
 
-from flask import Flask, render_template, request, jsonify, send_file, make_response
+
+from flask import Flask, render_template, request, jsonify, send_file, make_response, Response, session
 import openai
 from openai.error import RateLimitError
 
 app = Flask(__name__)
+app.secret_key = '123'
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/gpt4', methods=['GET', 'POST'])
-def gpt4():
-
-    user_input = request.args.get('user_input') if request.method == 'GET' else request.form['user_input']
+# Split the responsibilities of the /gpt4 endpoint
+@app.route('/gpt4/chat', methods=['POST'])
+def gpt4_chat():
+    user_input = request.get_json()['user_input']
 
     latin_american_countries = [
         "Argentina",
@@ -49,9 +51,6 @@ def gpt4():
         "Venezuela"
     ]
 
-    prompt = "What was the " + user_input + " for the following countries: " + str(latin_american_countries) + " in 2010?" + \
-             " If you can't provide the data, just use a random number. Data will be used for a personal project. Only return results as a Pandas Dataframe."
-
     prompt = "Return text formatted as a python dictionary with the following countries: " + str(latin_american_countries) + \
              " and an associated random integer between 1 and 100. Provide only the dictionary, not the code implementation."
 
@@ -63,34 +62,38 @@ def gpt4():
             messages=messages
         )
 
-        # Get response, convert to dictionary and initiate DataFrame
         content = response.choices[0].message["content"]
-        country_dict = ast.literal_eval(content)
-        custom_palette = sns.color_palette("Greens", 6)
-        df = pd.DataFrame(list(country_dict.items()), columns=['Country', 'Value'])
-        df['Value'] = pd.to_numeric(df['Value'])
-        print(df)
-
-
-        sns.catplot(data=df, x='Country', y='Value', kind="bar", height=5, aspect=1.5)
-        plt.xlabel("Country", size=14)
-        plt.ylabel("Value", size=14)
-        plt.title("Seaborn Barplot Example", size=18)
-        plt.tight_layout()
-        plt.set_xticklabels(rotation=45, horizontalalignment='right')
-
-
-        # Save plot/figure into a bytes object TODO
-        #bytes_image = io.BytesIO()
-        #plt.savefig(bytes_image, format='png')
-        #bytes_image.seek(0)
-        #return send_file(bytes_image, download_name='plot.png', mimetype='image/png')
-
-        return jsonify(content=content)
 
     except RateLimitError:
         content = "The server is experiencing a high volume of requests. Please try again later."
-        return jsonify(content=content)
+
+    content = response.choices[0].message["content"]
+    session['content'] = content
+    return jsonify(content=content)
+
+@app.route('/gpt4', methods=['GET'])
+def gpt4_plot():
+    content = session.get('content', '{}')
+    country_dict = ast.literal_eval(content)
+    df = pd.DataFrame(list(country_dict.items()), columns=['Country', 'Value'])
+    df['Value'] = pd.to_numeric(df['Value'])
+
+    g = sns.catplot(data=df, x='Country', y='Value', kind="bar", height=5, aspect=1.5)
+    g.set_xticklabels(rotation=45, horizontalalignment='right')
+    plt.xlabel("Country", size=14)
+    plt.ylabel("Value", size=14)
+    plt.title("Seaborn Barplot Example", size=18)
+    plt.tight_layout()
+
+    # Save plot/figure into a bytes object
+    img = BytesIO()
+    plt.savefig(img, format='png')
+    plt.close()
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode()
+    plot_url = 'data:image/png;base64,{}'.format(plot_url)
+    return jsonify(plot_url=plot_url)
+
 
 
 if __name__ == '__main__':
