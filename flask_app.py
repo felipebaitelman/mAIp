23074725b85daac4f82f24cb2e28e43b55b077b7
felipebaitelman <felipebaitelman@gmail.com>
2023-05-30 21:34:48 -0400

    Frontend adjustments in CSS and HTML

--- flask_app.py
+++ flask_app.py
@@ -6,6 +6,9 @@ from io import BytesIO
 import ast
 import matplotlib.pyplot as plt
 import matplotlib
+import matplotlib.font_manager as fm
+print(matplotlib.get_cachedir())
+from matplotlib import rcParams
 matplotlib.use('Agg')
 import base64
 
@@ -18,6 +21,12 @@ app = Flask(__name__)
 app.secret_key = '123'
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+# Add the font directory to the font manager path
+font_dir = './fonts'  # directory where your fonts are
+font_files = fm.findSystemFonts(fontpaths=font_dir)
+for font_file in font_files:
+    fm.fontManager.addfont(font_file)
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -26,7 +35,7 @@ def index():
 @app.route('/gpt4/chat', methods=['POST'])
 def gpt4_chat():
     user_input = request.get_json()['user_input']
-
+    session['user_input'] = user_input  # Save the user's input to the session
     latin_american_countries = [
         "Argentina",
         "Bolivia",
@@ -71,23 +80,43 @@ def gpt4_chat():
     session['content'] = content
     return jsonify(content=content)
 
+
 @app.route('/gpt4', methods=['GET'])
 def gpt4_plot():
+
+    # Change font of Seaborn chart
+    font_path = './fonts/Montserrat-Regular.ttf'  # replace with your .ttf file
+    font_prop = matplotlib.font_manager.FontProperties(fname=font_path, family='Montserrat')
+    matplotlib.rcParams['font.family'] = 'Montserrat'
+
     content = session.get('content', '{}')
     country_dict = ast.literal_eval(content)
     df = pd.DataFrame(list(country_dict.items()), columns=['Country', 'Value'])
     df['Value'] = pd.to_numeric(df['Value'])
 
-    g = sns.catplot(data=df, x='Country', y='Value', kind="bar", height=5, aspect=1.5)
+    g = sns.catplot(data=df, x='Country', y='Value', kind="bar", height=7, aspect=1.5)
     g.set_xticklabels(rotation=45, horizontalalignment='right')
-    plt.xlabel("Country", size=14)
-    plt.ylabel("Value", size=14)
-    plt.title("Seaborn Barplot Example", size=18)
+    user_input = session.get('user_input', 'Default Title')  # Retrieve the user's input from the session
+    plt.xlabel("Country", size=15, fontproperties=font_prop)
+    plt.ylabel("Value", size=15, fontproperties=font_prop)
+
     plt.tight_layout()
+
+    g.fig.suptitle(user_input, size=25, y=1, fontproperties=font_prop)
+
+    for ax in g.axes.flat:  # g.axes gives the list of all AxesSubplot objects
+        for label in ax.get_xticklabels():
+            label.set_fontproperties(font_prop)
+        for label in ax.get_yticklabels():
+            label.set_fontproperties(font_prop)
 
     # Save plot/figure into a bytes object
     img = BytesIO()
-    plt.savefig(img, format='png')
+
+    # Set the background to be transparent
+    matplotlib.rcParams['savefig.transparent'] = True
+
+    plt.savefig(img, format='png', dpi=300)
     plt.close()
     img.seek(0)
     plot_url = base64.b64encode(img.getvalue()).decode()
