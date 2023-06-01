fe93213878c1627d5ef9e51f37f3c5e1603c2a4f
felipebaitelman <felipebaitelman@gmail.com>
2023-06-01 16:49:52 -0400

    Implementation of multiple user inputs in frontend

--- static/js/index.js
+++ static/js/index.js
@@ -6,7 +6,14 @@ window.onload = function () {
             // Prevent the form from submitting and refreshing the page
             event.preventDefault();
 
-            let userInput = document.getElementById("user-input").value;
+            let userInput = document.getElementById("indicator").value;
+            let chartType = document.getElementById("chart-type").value;
+            let region = document.getElementById("region").value;
+            let countryInput = document.getElementById("country-input").value;
+
+            // Only include the country input if the "specific-country" option is selected
+            let postData = (region === "specific-country") ? { user_input: userInput, chart_type: chartType, region: region, country: countryInput } : { user_input: userInput, chart_type: chartType, region: region };
+
             let url = '/gpt4/chat';
 
             fetch(url, {
@@ -36,4 +43,38 @@ window.onload = function () {
                     console.error("Error fetching GPT-4 response:", error);
                 });
         });
+
+    // Set the initial state of the radio buttons
+    document.getElementById("time-series").checked = true;
+    document.getElementById("region").value = "specific-country";
+    document.getElementById("region").disabled = true;
+    document.getElementById("year-end").disabled = false;
+    document.getElementById("country-input").disabled = false;
+    document.getElementById("year-end").value = "";
+
 };
+
+// When the user changes the radio button to "One Year", lock the year-end radio button and clear the country input
+document.getElementById("one-year").addEventListener('change', function(event) {
+    document.getElementById("year-end").value = "";
+    document.getElementById("year-end").disabled = true;
+    document.getElementById("country-input").value = "";
+    document.getElementById("country-input").disabled = true;
+    document.getElementById("region").disabled = false;
+    let options = document.getElementById("region").options;
+    for (let i = 0; i < options.length; i++) {
+        if (options[i].value === "specific-country") {
+            options[i].remove();
+        }
+    }
+});
+
+// When the user changes the radio button to "Time Series", enable the "Year End" radio button
+document.getElementById("time-series").addEventListener('change', function(event) {
+    document.getElementById("region").options.add(new Option("Specific Country", "specific-country"));
+    document.getElementById("year-end").disabled = false;
+    document.getElementById("region").value = "specific-country";
+    document.getElementById("region").disabled = true;
+    document.getElementById("country-input").value = "";
+    document.getElementById("country-input").disabled = false;
+});
